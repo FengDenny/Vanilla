44ccc3c871ced11e14ed8d0069848120cf049090
Denny Feng <fengdennyy@gmail.com>
2023-05-04 11:17:05 -0700

    removed "--" to not get confused of using :root variables in css file

--- ThemeToggler/toggleStyle.js
+++ ThemeToggler/toggleStyle.js
@@ -1,13 +1,13 @@
 const themeToggler =  {
     darkTheme: {
-        "--darkTheme": " #212F3D",
-        "--lightTheme": "#F0F3F4",
-        "--darkThemeDescription": "#7189a3",
+        "darkTheme": " #212F3D",
+        "lightTheme": "#F0F3F4",
+        "darkThemeDescription": "#7189a3",
     }, 
     lightTheme: {
-        "--lightTheme": "#F0F3F4",
-        "--darkTheme": " #212F3D",
-        "--lightThemeDescription": "#3d4d5e",
+        "lightTheme": "#F0F3F4",
+        "darkTheme": " #212F3D",
+        "lightThemeDescription": "#3d4d5e",
     },
  
     body: document.querySelector('body'),
@@ -18,17 +18,17 @@ const themeToggler =  {
     currentTheme: "lightTheme",
     themeToggler: function() {
         const theme = this.currentTheme === "lightTheme" ? this.lightTheme : this.darkTheme;
-        this.body.style.backgroundColor = theme["--" + (this.currentTheme === "lightTheme" ? "light" : "dark") + "Theme"];
+        this.body.style.backgroundColor = theme[(this.currentTheme === "lightTheme" ? "light" : "dark") + "Theme"];
 
         // hero heading
-        this.headingTheme.style.setProperty("--lightTheme", this.lightTheme["--lightTheme"]);
-        this.headingTheme.style.setProperty("--darkTheme", this.darkTheme["--darkTheme"]);
-        this.headingTheme.style.color = theme['--' + (this.currentTheme === 'lightTheme' ? 'dark' : 'light') + 'Theme'];
+        this.headingTheme.style.setProperty("lightTheme", this.lightTheme["lightTheme"]);
+        this.headingTheme.style.setProperty("darkTheme", this.darkTheme["darkTheme"]);
+        this.headingTheme.style.color = theme[(this.currentTheme === 'lightTheme' ? 'dark' : 'light') + 'Theme'];
 
         // hero description
-        this.descriptionTheme.style.setProperty("--lightThemeDescription", this.lightTheme["--lightThemeDescription"]);
-        this.descriptionTheme.style.setProperty("--darkThemeDescription", this.darkTheme["--darkThemeDescription"]);
-        this.descriptionTheme.style.color = theme['--' + (this.currentTheme === 'lightTheme' ? 'light' : 'dark') + 'ThemeDescription'];
+        this.descriptionTheme.style.setProperty("lightThemeDescription", this.lightTheme["lightThemeDescription"]);
+        this.descriptionTheme.style.setProperty("darkThemeDescription", this.darkTheme["darkThemeDescription"]);
+        this.descriptionTheme.style.color = theme[(this.currentTheme === 'lightTheme' ? 'light' : 'dark') + 'ThemeDescription'];
 
         // theme toggle buttons
         this.lightThemeButton.style.display = this.currentTheme === "lightTheme" ? "none" : "block";
@@ -51,9 +51,9 @@ function currentTheme() {
     const currentTheme = themeToggler.currentTheme;
     themeToggler.lightThemeButton.style.display = currentTheme === 'lightTheme' ? "none" : "block";
     themeToggler.darkThemeButton.style.display = currentTheme === 'lightTheme' ? "block" : "none";
-    themeToggler.body.style.backgroundColor = themeToggler[currentTheme]["--" + (currentTheme === "lightTheme" ? "light" : "dark") + "Theme"];
-    themeToggler.headingTheme.style.color = themeToggler[currentTheme]["--" + (currentTheme === "lightTheme" ? "dark" : "light") + "Theme"];
-    themeToggler.descriptionTheme.style.color = themeToggler[currentTheme]["--" + (currentTheme === "lightTheme" ? "light" : "dark") + "ThemeDescription"];
+    themeToggler.body.style.backgroundColor = themeToggler[currentTheme][(currentTheme === "lightTheme" ? "light" : "dark") + "Theme"];
+    themeToggler.headingTheme.style.color = themeToggler[currentTheme][(currentTheme === "lightTheme" ? "dark" : "light") + "Theme"];
+    themeToggler.descriptionTheme.style.color = themeToggler[currentTheme][ (currentTheme === "lightTheme" ? "light" : "dark") + "ThemeDescription"];
 }
 
 currentTheme();
