c7a868218e493e7df6052018a0bd06a613fd3e33
Denny Feng <fengdennyy@gmail.com>
2023-05-09 13:58:02 -0700

    Updated code to use IIFE for code encapsulation

--- Gallery/photoGallery.js
+++ Gallery/photoGallery.js
@@ -1,37 +1,50 @@
-const photoGallery = {
-    photos: [],
-    galleryGrid: document.querySelector(".gallery-grid"),
-    endpoint:"https://jsonplaceholder.typicode.com/photos" ,
-    init() {
-      this.fetchPhotos();
-    },
-    fetchPhotos() {
-      fetch(`${this.endpoint}?_limit=52`)
-        .then(response => response.json())
-        .then(data => {
-          this.photos = data;
-          this.createMasonry();
-        })
-        .catch(error => console.error(error));
-    },
-    createPhotoGallery(photo) {
-      const { title, thumbnailUrl } = photo;
-      const html = `
+const photoGallery = (function() {
+  function init() {
+    this.fetchPhotos();
+  }
+
+  function fetchPhotos() {
+    fetch(this.endpoint)
+      .then(function(response) {
+        return response.json();
+      }.bind((this)))
+      .then(function(data) {
+        this.photos = data;
+        this.createMasonry();
+      }.bind(this))
+      .catch(function(error) {
+        console.error(error);
+      });
+  }
+
+  function createPhotoGallery(photo) {
+    const { title, thumbnailUrl } = photo;
+    const html = `
       <div class="photo" data-title="${title}" data-thumbnail="${thumbnailUrl}">
-      <img src="${thumbnailUrl}" alt="${title}">
-      <div class="title-overlay">
+        <img src="${thumbnailUrl}" alt="${title}">
+        <div class="title-overlay">
           <p>${title}</p>
         </div>
-    </div>`;
-      return html;
-    },
-    createMasonry() {
-      this.photos.forEach((photo) => {
-        const gallery = this.createPhotoGallery(photo);
-        this.galleryGrid.insertAdjacentHTML("beforeend", gallery);
-      });
-    },
+      </div>`;
+    return html;
+  }
+
+  function createMasonry() {
+    this.photos.forEach(function(photo) {
+      const gallery = this.createPhotoGallery(photo);
+      this.galleryGrid.insertAdjacentHTML("beforeend", gallery);
+    }.bind(this));
+  }
+
+  return {
+    photos: [],
+    endpoint:"https://jsonplaceholder.typicode.com/photos?_limit=50",
+    galleryGrid: document.querySelector(".gallery-grid"),
+    init,
+    fetchPhotos,
+    createPhotoGallery,
+    createMasonry
   };
-  
-  photoGallery.init();
-  
+})();
+
+photoGallery.init();
