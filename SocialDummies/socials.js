0a76a113666fc015e1e794bb376968b2a563d82c
Denny Feng <fengdennyy@gmail.com>
2023-05-12 12:54:24 -0700

    Created comments accordion section for each post

--- SocialDummies/socials.js
+++ SocialDummies/socials.js
@@ -1,11 +1,14 @@
 const socialsPost = (function(){
     function init(){
         this.fetchAll()
+        this.addCardButtonListener()
     }
     async function fetchAll(){
-      try{
+      try{      
+        this.showSpinner()
         const response = await fetch(this.allPosts)
         const posts = await response.json()
+
         // Iterate over each post and fetch comments
         for (let post of posts){
             const commentsResponse = await fetch(`${this.allPosts}${post.id}/comments`)
@@ -18,6 +21,7 @@ const socialsPost = (function(){
         this.posts = posts.slice(startIndex, endIndex)
         this.start += this.limit
         this.createCard()
+  
         this.hideSpinner()
         this.fetching = false
 
@@ -29,13 +33,14 @@ const socialsPost = (function(){
     function createCardContent(posts){
         const {title, body, id} = posts
         const html =  `<div class="card" data-id="${id}" data-title="${title}" data-body="${body}">
-        <div>
+        <div class="card-header">
             <h2 class="description-text-white">${title}</h2>
             <p class="description-text">${body}</p>
         </div>
         <div class="card-btn">
             <button id="view-comments" class="card-btn-view">View Comments</button>
         </div>
+        <div id="comments"></div>
     </div>
      `
     return html
@@ -52,6 +57,31 @@ function createCard(){
     this.setupIntersectionObserver()
 
 }
+
+function toggleComments(e) {
+    const button = e.target
+    const card = button.closest(".card")
+    const commentsSection = card.querySelector("#comments")
+
+    if(commentsSection.children.length === 0){
+        const postId = card.dataset.id
+        const post = this.posts.find(p =>Number(p.id) === Number(postId))
+        console.log(post)
+        if(post.comments !== undefined){
+            post.comments.forEach((comment) => {
+                const html = `<div class="comment">
+                <h2 class="comment-email description-text">${comment.email}</h2>
+                <p class="comment-body description-text-white">${comment.body}</p>
+            </div>`
+            commentsSection.insertAdjacentHTML("beforeend", html)
+            })
+        }
+    }
+    commentsSection.classList.toggle("visible")
+    if(!commentsSection.classList.contains("visible")) commentsSection.textContent =""
+    button.textContent = commentsSection.classList.contains("visible") ? "Hide Comments" : "View Comments"
+}
+
 
 function setupIntersectionObserver(){
     const options ={
@@ -73,12 +103,18 @@ function setupIntersectionObserver(){
 }
 
 function showSpinner(){
-    this.loadingItems.classList.add(" spinner")
+    this.loadingItems.classList.add("spinner")
 }
 function hideSpinner(){
     this.loadingItems.classList.remove("spinner")
 }
 
+function addCardButtonListener() {
+    document.addEventListener("click", function(event){
+        const button = event.target
+        if(button.classList.contains("card-btn-view")) socialsPost.toggleComments(event)   
+    })
+}
 
 return{
     posts:[],
@@ -95,9 +131,12 @@ return{
     createCard,
     setupIntersectionObserver,
     showSpinner,
-    hideSpinner
+    hideSpinner,
+    toggleComments,
+    addCardButtonListener
 }
 
 })()
 
 socialsPost.init()
+
